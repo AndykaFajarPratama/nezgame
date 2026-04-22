30919e4a04cd023cb67f1cede41986c1b7b2ea20
AndykaFajarPratama <feroznez@gmail.com>
2026-04-22 10:48:38 +0700

    fix: update proxy config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -11,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:3000', // ✅ Pakai env
         changeOrigin: true,
       }
     }
